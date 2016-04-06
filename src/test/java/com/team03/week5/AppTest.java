aa1a30ee4909dfd835571bfc5b56954b5ead0958
Kohyunmin <hyunmin4578@gmail.com>
2016-04-06 16:46:40 +0900

    resolve to JUnit test

--- src/test/java/com/team03/week5/AppTest.java
+++ src/test/java/com/team03/week5/AppTest.java
@@ -1,38 +1,13 @@
 package com.team03.week5;
+import static org.junit.Assert.*;
+import org.junit.*;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
-    extends TestCase
+
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
+   @Test
     public void testApp()
     {
-        assertTrue( true );
+        assertEquals("","" );
     }
 }
