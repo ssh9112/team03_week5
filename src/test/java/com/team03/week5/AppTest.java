980523c91ec2a6b97d0fa9ca6be4e7c794693fdf
Kohyunmin <hyunmin4578@gmail.com>
2016-04-06 17:09:07 +0900

    modifyed pom.xml

--- src/test/java/com/team03/week5/AppTest.java
+++ src/test/java/com/team03/week5/AppTest.java
@@ -2,12 +2,9 @@ package com.team03.week5;
 import static org.junit.Assert.*;
 import org.junit.*;
 
-public class AppTest 
-
-{
+public class AppTest{
    @Test
-    public void testApp()
-    {
+    public void testApp(){
         assertEquals("","" );
     }
 }
