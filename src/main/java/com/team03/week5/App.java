ebf7a986d62239155e049546c53f351b3418cb1b
Jeongsoo Ha <jeongsoo931210@gmail.com>
2016-04-10 23:01:04 +0900

    remove App constructor

--- src/main/java/com/team03/week5/App.java
+++ src/main/java/com/team03/week5/App.java
@@ -12,9 +12,7 @@ public class App {
 	private static final Logger LOGGER = Logger.getLogger("InfoLogging");
 	private static final String PRINTERRORMESSAGE = "잘못된 입력값입니다.";
 
-	public App() {
-		//nothing to do;
-	}
+	
 
 	public static void main(String[] args) {
 
